14ae980c196284da72a7134a9e5940810f40fdc6
unknown <prayagvadodariya@gmail.com>
2021-05-29 17:18:40 +0530

    all quiz add complete

--- src/constant/StaticData.js
+++ src/constant/StaticData.js
@@ -22,55 +22,486 @@ export const QuizList=[
           ]
         },
         {
-          question: " localhost's IP address?",
+          question: "Who is the Father of the Computer?",
           answers: [
-            { id: "1", text: "192.168.1.1" },
-            { id: "2", text: "127.0.0.1", correct: true },
-            { id: "3", text: "209.85.231.104" },
-            { id: "4", text: "66.220.149.25" }
+            { id: "1", text: "Charles Babbage", correct: true },
+            { id: "2", text: "Thomas Edison" },
+            { id: "3", text: "Albert Einstein" },
+            { id: "4", text: "Isaac Newton" }
+          ]
+        },
+        {
+          question: "What is the full form of E-Mail?",
+          answers: [
+            { id: "1", text: "Electric Mail" },
+            { id: "2", text: "Exchange Mail" },
+            { id: "3", text: "Electronic Mail", correct: true },
+            { id: "4", text: "Engagement Mail" }
+          ]
+        },
+        {
+          question: "In the virtual world, WWW stands for ______",
+          answers: [
+            { id: "1", text: "World Without Windows" },
+            { id: "2", text: "World Wide Web Applications" },
+            { id: "3", text: "World Wide Warehouse" },
+            { id: "4", text: "World Wide Web", correct: true }
+          ]
+        },
+        {
+          question: "Which one of the following is not an Operating System (OS)?",
+          answers: [
+            { id: "1", text: "Windows 10" },
+            { id: "2", text: "MS Excel", correct: true },
+            { id: "3", text: "Linux" },
+            { id: "4", text: "DOS" }
+          ]
+        },
+        {
+          question: "What is also known as a portable computer?",
+          answers: [
+            { id: "1", text: "Desktop" },
+            { id: "2", text: "Monitor" },
+            { id: "3", text: "CPU" },
+            { id: "4", text: "Laptop", correct: true }
+          ]
+        },
+        {
+          question: "How much is a byte equal to?",
+          answers: [
+            { id: "1", text: "8 bits", correct: true },
+            { id: "2", text: "16 bits" },
+            { id: "3", text: "32 bits" },
+            { id: "4", text: "64 bits" }
           ]
         },
       ]
     },
     { id: 2,
       title: 'Literature',  
-      image: require('../assets/images/2.png')
+      image: require('../assets/images/2.png'),
+      Quiz:[
+        {
+          question: "The protagonist of several Dan Brown novels, Robert Landon teaches at what university?",
+          answers: [
+            { id: "1", text: "GTU University" },
+            { id: "2", text: "Harvard University", correct: true },
+            { id: "3", text: "RK University" },
+            { id: "4", text: "Trine University" }
+          ]
+        },
+        {
+          question: "Characters live in West Egg and East Egg in what F. Scott Fitzgerald novel?",
+          answers: [
+            { id: "1", text: "The Great" },
+            { id: "2", text: "Gatsby" },
+            { id: "3", text: "The ant" },
+            { id: "4", text: "The Great Gatsby", correct: true}
+          ]
+        },
+        {
+          question: "The Jungle Book features what man cub?",
+          answers: [
+            { id: "1", text: "Mowgli", correct: true},
+            { id: "2", text: "Odyssey" },
+            { id: "3", text: "Rabbit" },
+            { id: "4", text: "John F" }
+          ]
+        },
+        {
+          question: "Who wrote Uncle Tom’s Cabin?",
+          answers: [
+            { id: "1", text: "C.S. Lewis" },
+            { id: "2", text: "Jack London" },
+            { id: "3", text: "Harriet Beecher Stowe", correct: true},
+            { id: "4", text: "Jules Verne" }
+          ]
+        },
+        {
+          question: "Achilles killed Hector in single combat in what epic poem?",
+          answers: [
+            { id: "1", text: "Ili" },
+            { id: "2", text: "Iliad", correct: true},
+            { id: "3", text: "Ilied" },
+            { id: "4", text: "Iliads" }
+          ]
+        },
+        {
+          question: "The nickname “The Scottish Play” refers to what Shakespearean play?",
+          answers: [
+            { id: "1", text: "Odyssey" },
+            { id: "2", text: "Jules Verne" },
+            { id: "3", text: "Macbeth", correct: true},
+            { id: "4", text: "Lewis" }
+          ]
+        },
+        {
+          question: "Who wrote Cat on a Hot Tin Roof?",
+          answers: [
+            { id: "1", text: "Tennessee Williams", correct: true},
+            { id: "2", text: "Williams" },
+            { id: "3", text: "Jack S" },
+            { id: "4", text: "Jon" }
+          ]
+        },
+        {
+          question: "What popular character first appeared as the protagonist of an 1883 children’s novel by Italian author Carlo Collodi?",
+          answers: [
+            { id: "1", text: "shoq" },
+            { id: "2", text: "Jack London" },
+            { id: "3", text: "Holmes" },
+            { id: "4", text: "Pinocchio", correct: true}
+          ]
+        },
+        {
+          question: "Who wrote The Chronicles of Narnia?",
+          answers: [
+            { id: "1", text: "John F. Kennedy" },
+            { id: "2", text: "Lewis" },
+            { id: "3", text: "C.S. Lewis", correct: true},
+            { id: "4", text: "Holmes" }
+          ]
+        },
+        {
+          question: "Who wrote 20,000 Leagues Under the Sea?",
+          answers: [
+            { id: "1", text: "Maldives" },
+            { id: "2", text: "Verne" },
+            { id: "3", text: "Jules Verne", correct: true},
+            { id: "4", text: "Jules" }
+          ]
+        },
+      ]  
     },
     { id: 3,
       title: 'Geography',  
-      image: require('../assets/images/3.png') 
+      image: require('../assets/images/3.png'),
+      Quiz:[
+        {
+          question: "Which state is going to observe “No Vehicle Day” on the first day of every month in 2020?",
+          answers: [
+            { id: "1", text: "Kerala" },
+            { id: "2", text: "Rajasthan", correct: true },
+            { id: "3", text: "Gujarat" },
+            { id: "4", text: "Punjab" }
+          ]
+        },
+        {
+          question: "Which is the first country to ban sun screens, that are toxic to coral reefs and sea life?",
+          answers: [
+            { id: "1", text: "Philippines" },
+            { id: "2", text: "Taiwan" },
+            { id: "3", text: "Palau", correct: true},
+            { id: "4", text: "Papua New Guinea" }
+          ]
+        },
+        {
+          question: "Which state will have the country’s first rehabilitation centre for freshwater turtles?",
+          answers: [
+            { id: "1", text: "Madhya Pradesh" },
+            { id: "2", text: "Bihar", correct: true },
+            { id: "3", text: "Chhattisgarh" },
+            { id: "4", text: "Odisha" }
+          ]
+        },
+        {
+          question: "Which Indian state has recently radio-tagged an Indian Pangolin, on the occasion of World Pangolin Day?",
+          answers: [
+            { id: "1", text: "Madhya Pradesh", correct: true},
+            { id: "2", text: "Uttarakhand" },
+            { id: "3", text: "Andhra Pradesh" },
+            { id: "4", text: "Kerala" }
+          ]
+        },
+        {
+          question: "Trionychidae, that was seen in news recently, belongs to which species?",
+          answers: [
+            { id: "1", text: "Spider" },
+            { id: "2", text: "Snake" },
+            { id: "3", text: "Owl" },
+            { id: "4", text: "Turtle", correct: true}
+          ]
+        },
+        {
+          question: "In which state, a new species of fish named ‘Schizothorax sikusirumensis’ has been discovered?",
+          answers: [
+            { id: "1", text: "West Bengal" },
+            { id: "2", text: "Arunachal Pradesh", correct: true },
+            { id: "3", text: "Maharashtra" },
+            { id: "4", text: "Kerala" }
+          ]
+        },
+      ]
     },
     { id: 4,
       title: 'History',  
-      image: require('../assets/images/4.png') 
+      image: require('../assets/images/4.png'),
+      Quiz:[
+        {
+          question: "Giza Pyramid and the Great Sphinx",
+          answers: [
+            { id: "1", text: "Kuwait" },
+            { id: "2", text: "Egypt", correct: true },
+            { id: "3", text: "United Kingdom" },
+            { id: "4", text: "Iran" }
+          ]
+        },
+        {
+          question: "Which Country Found Taj Mahal",
+          answers: [
+            { id: "1", text: "Cambodia" },
+            { id: "2", text: "India", correct: true },
+            { id: "3", text: "North Korea" },
+            { id: "4", text: "Australia" }
+          ]
+        },
+        {
+          question: "Juche Tower Found Country Name?",
+          answers: [
+            { id: "1", text: "Egypt" },
+            { id: "2", text: "Kuwait" },
+            { id: "3", text: "North Korea", correct: true },
+            { id: "4", text: "America" }
+          ]
+        },
+        {
+          question: "Which Buddhist monk converted Milinda (Indo-Greek king) to Buddhism?",
+          answers: [
+            { id: "1", text: "Gautam Budha" },
+            { id: "2", text: "Nagasena", correct: true },
+            { id: "3", text: "Shakyamuni" },
+            { id: "4", text: "Mahadharmaraksita" }
+          ]
+        },
+        {
+          question: "Metronymics were borne by the later rulers of which among the following dynasties?",
+          answers: [
+            { id: "1", text: "Satavahana" , correct: true},
+            { id: "2", text: "Sunga" },
+            { id: "3", text: "Kanva" },
+            { id: "4", text: "Maurya" }
+          ]
+        },
+        {
+          question: "According to some Jain traditions, who was son-in-law and first disciple of Tirthankara Mahavira?",
+          answers: [
+            { id: "1", text: "Jamval" },
+            { id: "2", text: "Cant’ say" },
+            { id: "3", text: "Jamvant" },
+            { id: "4", text: "Jamali" , correct: true}
+          ]
+        },
+        {
+          question: "Dashkumarcharitam was composed by whom?",
+          answers: [
+            { id: "1", text: "Bharavi" },
+            { id: "2", text: "Dandin", correct: true },
+            { id: "3", text: "Varahmihira" },
+            { id: "4", text: "Kalidas" }
+          ]
+        },
+        {
+          question: "Which rock edict gives information about Asoka’s conversion to Buddhism?",
+          answers: [
+            { id: "1", text: "Kalinga rock edict" },
+            { id: "2", text: "Tarai rock edict" },
+            { id: "3", text: "Bhabru rock edict", correct: true },
+            { id: "4", text: "Barabar cave rock edict" }
+          ]
+        },
+        {
+          question: "Who was the mother of Gautamiputra Satkarni?",
+          answers: [
+            { id: "1", text: "Shubhadrangi" },
+            { id: "2", text: "Nirjara" },
+            { id: "3", text: "Koshala" },
+            { id: "4", text: "Gautami" , correct: true }
+          ]
+        },
+        {
+          question: "Who overthrew Saka rulers from India?",
+          answers: [
+            { id: "1", text: "Kanishka" },
+            { id: "2", text: "Gautami putra Satakarni", correct: true },
+            { id: "3", text: "Rudraraman" },
+            { id: "4", text: "Demetrius" }
+          ]
+        },
+      ]  
     },
     { id: 5,
-      title: 'Pop culture',  
-      image: require('../assets/images/5.png') 
+      title: 'Sports',  
+      image: require('../assets/images/5.png'),
+      Quiz:[
+        {
+          question: "Which Indian city hosts the All India Police Judo Cluster Championship 2019?",
+          answers: [
+            { id: "1", text: "New Delhi", correct: true },
+            { id: "2", text: "Cuttack" },
+            { id: "3", text: "Guwahati" },
+            { id: "4", text: "Goa" }
+          ]
+        },
+        {
+          question: "Which Indian Grandmaster won the title in the Hastings International Chess Congress?",
+          answers: [
+            { id: "1", text: "Pendyala Harikrishna" },
+            { id: "2", text: "Sasikiran Krishnan" },
+            { id: "3", text: "P Magesh Chandran", correct: true },
+            { id: "4", text: "Abhijeet Gupta" }
+          ]
+        },
+        {
+          question: "Who clinched the men’s singles title in the recently held Australian Open tournament, 2020?",
+          answers: [
+            { id: "1", text: "Novak Djokovic", correct: true },
+            { id: "2", text: "Roger Federer" },
+            { id: "3", text: "Rafael Nadal" },
+            { id: "4", text: "Dominic Thiem" }
+          ]
+        },
+        {
+          question: "Rakhi Halder, who was seen in news recently, is associated with which sports",
+          answers: [
+            { id: "1", text: "Hockey" },
+            { id: "2", text: "Weight-lifting", correct: true },
+            { id: "3", text: "Cricket" },
+            { id: "4", text: "Wrestling" }
+          ]
+        },
+        {
+          question: "Aditya Mehta, who was seen in news recently, is associated with sports?",
+          answers: [
+            { id: "1", text: "Weight-lifting" },
+            { id: "2", text: "Archery" },
+            { id: "3", text: "Boxing" },
+            { id: "4", text: "Snooker", correct: true }
+          ]
+        },
+        {
+          question: "Which team bagged the 9th National Ice Hockey Championship, 2020 title?",
+          answers: [
+            { id: "1", text: "Indian Army Red", correct: true },
+            { id: "2", text: "Maharashtra" },
+            { id: "3", text: "Indo Tibetan Border Police" },
+            { id: "4", text: "Indian Army Blue" }
+          ]
+        },
+        {
+          question: "The 2024 Summer Olympics is scheduled to be held in which city?",
+          answers: [
+            { id: "1", text: "Beijing" },
+            { id: "2", text: "London" },
+            { id: "3", text: "Paris", correct: true },
+            { id: "4", text: "Los Angeles" }
+          ]
+        },
+        {
+          question: "Who is the first Indian to win the Fed Cup Heart Award for Asia/Oceania zone?",
+          answers: [
+            { id: "1", text: "Sania Mirza", correct: true },
+            { id: "2", text: "Leander Paes" },
+            { id: "3", text: "Rohan Bopanna" },
+            { id: "4", text: "Prajnesh Gunneswaran" }
+          ]
+        },
+      ] 
     },
     { id: 6,
       title: 'Science',  
-      image: require('../assets/images/6.png') 
+      image: require('../assets/images/6.png'),
+      Quiz:[
+        {
+          question: "Which among the following Vitamins helps in clotting of Blood?",
+          answers: [
+            { id: "1", text: "Vitamin K", correct: true },
+            { id: "2", text: "Vitamin E" },
+            { id: "3", text: "Vitamin D" },
+            { id: "4", text: "Vitamin C" }
+          ]
+        },
+        {
+          question: "In which among the following diseases, Seed Therapy is used now a days for treatment?",
+          answers: [
+            { id: "1", text: "AIDS" },
+            { id: "2", text: "Cancer", correct: true },
+            { id: "3", text: "Tuberculosis" },
+            { id: "4", text: "Asthama" }
+          ]
+        },
+        {
+          question: "“Gnathology” is the study of which among the following?",
+          answers: [
+            { id: "1", text: "Masticatory System", correct: true },
+            { id: "2", text: "Teeth" },
+            { id: "3", text: "Jaws" },
+            { id: "4", text: "Tongue" }
+          ]
+        },
+        {
+          question: "Amflora is a genetically modified ______?",
+          answers: [
+            { id: "1", text: "Rose" },
+            { id: "2", text: "Lotus" },
+            { id: "3", text: "Potato", correct: true },
+            { id: "4", text: "Tomato" }
+          ]
+        },
+        {
+          question: "Which among the following clearly describes a Silver Fish?",
+          answers: [
+            { id: "1", text: "A Fish", correct: true },
+            { id: "2", text: "A wingless Insect" },
+            { id: "3", text: "An Anthropod" },
+            { id: "4", text: "A frog" }
+          ]
+        },
+        {
+          question: "Which gas is used in making of Vanaspati ghee from vegetable oils?",
+          answers: [
+            { id: "1", text: "N2" },
+            { id: "2", text: "NO2" },
+            { id: "3", text: "H2" },
+            { id: "4", text: "H2", correct: true }
+          ]
+        },
+        {
+          question: "Haematite & Magnetite are ______?",
+          answers: [
+            { id: "1", text: "Copper Ores" },
+            { id: "2", text: "Iron Ores", correct: true },
+            { id: "3", text: "Manganese Ores" },
+            { id: "4", text: "Zinc Ores" }
+          ]
+        },
+        {
+          question: "Which acid is described as HOOCCOOH?",
+          answers: [
+            { id: "1", text: "Carbonic Acid" },
+            { id: "2", text: "Acetic Acid" },
+            { id: "3", text: "Oxalic Acid", correct: true },
+            { id: "4", text: "Citric Acid" }
+          ]
+        },
+        {
+          question: "Which of the following is also known as Alumina?",
+          answers: [
+            { id: "1", text: "Al2O3", correct: true },
+            { id: "2", text: "AlO2" },
+            { id: "3", text: "Al2O4" },
+            { id: "4", text: "Al2CO2" }
+          ]
+        },
+        {
+          question: "How many values an Electron spin quantum number can have?",
+          answers: [
+            { id: "1", text: "2", correct: true },
+            { id: "2", text: "1" },
+            { id: "3", text: "4" },
+            { id: "4", text: "8" }
+          ]
+        },
+      ] 
     }
   ]
 
-  // export const ComputerQuiz = [
-  //   {
-  //     question: "What is localhost's IP address?",
-  //     answers: [
-  //       { id: "1", text: "192.168.1.1" },
-  //       { id: "2", text: "127.0.0.1", correct: true },
-  //       { id: "3", text: "209.85.231.104" },
-  //       { id: "4", text: "66.220.149.25" }
-  //     ]
-  //   },
-  //   {
-  //     question: "What kind of fruit was used to name a computer in 1984?",
-  //     answers: [
-  //       { id: "1", text: "Blackberry" },
-  //       { id: "2", text: "Blueberry" },
-  //       { id: "3", text: "Pear" },
-  //       { id: "4", text: "Apple", correct: true }
-  //     ]
-  //   }
-  // ];
