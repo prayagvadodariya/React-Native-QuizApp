cdb4a07ecaed9cb39f534503361018551b5d7588
unknown <prayagvadodariya@gmail.com>
2021-06-02 12:57:59 +0530

    update code

--- src/Screens/Add_Quiz_Categories.js
+++ src/Screens/Add_Quiz_Categories.js
@@ -1,13 +1,15 @@
 import { useLinkProps } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, SafeAreaView, TextInput, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, TextInput, Image, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
 import firebase from '../services/firebaseServices';
-
+import { AntDesign, Entypo, Ionicons, Feather, EvilIcons } from 'react-native-vector-icons';
+import * as ImagePicker from 'expo-image-picker';
 
 const Add_Quiz_Categories = (props) => {
   const [quiztitile, onQuizTitle] = useState('');
+  const [image, setImage] = useState(null);
 
   useEffect(() => {
     const dbRef = firebase.database().ref();
@@ -22,15 +24,36 @@ const Add_Quiz_Categories = (props) => {
     });
   },[])
 
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+    const ref = firebase.storage().ref().child(quiztitile);
+    var url =   result.uri
+    ref.putString(url, 'data_url').then((snapshot) => {
+      console.log(snapshot,'Uploaded a data_url string!');
+    });
+
+      setImage(result.uri);
+    }
+  };
 
   const onAddQuizCategories = () => {
-    firebase
-    .database()
-    .ref('quizList/' + new Date().valueOf())
-    .set({
-      title: quiztitile,  
-      image: require('../assets/images/1.png'),
-    });
+  
+    // firebase
+    // .database()
+    // .ref('quizList/' + new Date().valueOf())
+    // .set({
+    //   title: quiztitile,  
+    //   image: image,
+    // });
     props.navigation.navigate("BottomTabStack", { screen: 'Home',params: {first: 'add'}})
   }
 
@@ -48,13 +71,24 @@ const Add_Quiz_Categories = (props) => {
           />
         </View>
 
+        <View style={{flexDirection:'row'}}>
+          <Text style={styles.t1}>Upload Quiz Image</Text>
+          <AntDesign style={styles.upload_icon} name="clouduploado" color='#06d3f7' size={36} onPress={pickImage}/>
+        </View>
+
+        {image ?
+          <View style={{flex:1,alignContent:'center',alignItems:'center'}}>
+            <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+          </View>
+        :null}
+
         <View style={{flex:1,alignContent:'center',alignItems:'center'}}>
           <Button
             title="Add Quiz Categories"
             type="solid"
             onPress={()=> onAddQuizCategories()}
             titleStyle={{textAlign:'center'}}
-            buttonStyle={{borderRadius:10, marginBottom:10, marginTop:15, backgroundColor:'#06d3f7'}}
+            buttonStyle={{borderRadius:10, marginBottom:10, marginTop:5, backgroundColor:'#06d3f7'}}
           />
         </View>
 
@@ -80,13 +114,17 @@ const styles = StyleSheet.create({
   input: {
     marginLeft:20,
     marginRight:20,
-    // marginBottom:20,
     height:50,
     fontSize:20,
     borderColor:'#19203c',
     borderRadius:10,
     borderWidth:1.5,
     paddingLeft:10
+  },
+  upload_icon: {
+    marginTop:15,
+    marginLeft:20,
+    marginBottom:10
   }
 });
 
--- src/services/firebaseServices.js
+++ src/services/firebaseServices.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import "@firebase/database";
+import "@firebase/storage";
 // Optionally import the services that you want to use
 //import "firebase/auth";
 //import "firebase/database";
